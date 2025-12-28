7bdc9ef053b8123dbfd53510c602c649bac96276
Likith Naidu <likith.anumakonda@gamil.com>
2025-12-28 10:24:08 +0530

    Fix: Ignore build errors

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
